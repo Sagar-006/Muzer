03e9f94444bb5c5f45e6ddca415fb2f9eba453b1
Sagar-006 <sagarbiradar7030@gmail.com>
2025-09-22 23:50:35 +0530

    Added ui components

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -1,34 +1,69 @@
 "use client";
-import {signIn, signOut, useSession} from "next-auth/react";
-import { Button } from "./ui/button";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import Link from "next/link";
+import { ThemeSwitcher } from "@/app/ThemeSwitcher";
 
-export function Appbar  ()  {
-    const session = useSession();
-    return (
-      <div className="flex justify-between px-20 py-2 border-2 relative z-50">
-        <div className="flex justify-between w-full">
-          <h1 className="font-semibold text-xl text-white">MUZER</h1>
-          <div>
-            {session.data?.user && (
-              <Button
-                variant={"secondary"}
-                className=""
-                onClick={() => signOut()}
-              >
-                logout
-              </Button>
-            )}
-            {!session.data?.user && (
-              <Button
-                variant={"secondary"}
-                className="bg-purple-800 text-white hover:bg-purple-900"
-                onClick={() => signIn()}
-              >
-                signIn
-              </Button>
-            )}
-          </div>
-        </div>
+export function Appbar({ showThemeSwitch = true, isSpectator = false }) {
+  const session = useSession();
+  const router = useRouter();
+
+  return (
+    <div className="flex justify-between px-5 py-4 md:px-10 xl:px-20">
+      <div
+        onClick={() => {
+          router.push("/home");
+        }}
+        className={`flex flex-col justify-center text-lg font-bold hover:cursor-pointer ${
+          showThemeSwitch ? "" : "text-white"
+        }`}
+      >
+        Muzer
       </div>
-    );
+      <div className="flex items-center gap-x-2">
+        {isSpectator && <WalletMultiButton />}
+        {session.data?.user && (
+          <Button
+            className="bg-purple-600 text-white hover:bg-purple-700"
+            onClick={() =>
+              signOut({
+                callbackUrl: "/",
+              })
+            }
+          >
+            Logout
+          </Button>
+        )}
+        {!session.data?.user && (
+          <div className="space-x-3">
+            <Button
+              className="bg-purple-600 text-white hover:bg-purple-700"
+              onClick={() => router.push("/auth")}
+            >
+              Signin
+            </Button>
+            <Link
+              href={{
+                pathname: "/auth",
+                query: {
+                  authType: "signUp",
+                },
+              }}
+            >
+              <Button
+                variant={"ghost"}
+                className="text-white hover:bg-white/10"
+              >
+                Signup
+              </Button>
+            </Link>
+          </div>
+        )}
+
+        {showThemeSwitch && <ThemeSwitcher />}
+      </div>
+    </div>
+  );
 }
